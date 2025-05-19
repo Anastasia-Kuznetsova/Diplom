f5bbdf54815855ab61ddd5dc462bc218792f0b97
Anastasia Kuznetsova <125804284+Anastasia-Kuznetsova@users.noreply.github.com>
2025-05-19 08:42:43 +0500

    Update math_filters.py

--- templatetags/math_filters.py
+++ templatetags/math_filters.py
@@ -5,24 +5,34 @@ register = template.Library()
 
 @register.filter(name='convert_to_latex')
 def convert_to_latex(text):
-    """
-    Преобразует текст в LaTeX, если он содержит формулу.
-    Автоматически преобразует простые математические выражения в LaTeX.
-    """
-    text = text.replace("минус", "-")
-    #text = text.replace("умножить", r"*")
-    text = text.replace("умножить на", r" * ")
-    text = text.replace("умножить", r" * ") 
-    text = text.replace("плюс", "+")
-    text = text.replace("разделить", "/")
-    text = text.replace("в степени", "^")
-    text = text.replace(",", ".")
-    text = text.replace("sin", r"\mathrm{sin}")
-    text = text.replace("cos", r"\mathrm{cos}")
-    text = text.replace("tan", r"\mathrm{tan}")
-    text = text.replace("ctg", r"\mathrm{ctg}")
-    text = text.replace("левая круглая скобка", "(")  
-    text = text.replace("правая круглая скобка", ")")
-    text = re.sub(r"\\left\|(.*?)\\right\|", r"\\left|\\text{\1}\\right|", text)
+    if re.search(r"\\\w+", text):
+        return f"${text}$"
+    else:
+        text = text.replace("минус", "-")
+        text = text.replace("умножить на", r" * ")
+        text = text.replace("умножить", r" * ")
+        text = text.replace("плюс", "+")
+        text = text.replace("разделить", "/")
+        text = text.replace("в степени", "^")
+        text = text.replace(",", ".")
+        text = text.replace("sin", r"\mathrm{sin}")
+        text = text.replace("cos", r"\mathrm{cos}")
+        text = text.replace("tan", r"\mathrm{tan}")
+        text = text.replace("ctg", r"\mathrm{ctg}")
+        text = text.replace("левая круглая скобка", "(")
+        text = text.replace("правая круглая скобка", ")")
+        text = re.sub(r"\\left\|(.*?)\\right\|", r"\\left|\\text{\1}\\right|", text)
 
-    return f"$\\text{{{text}}}$"
+        parts = re.split(r"([+\-*\/^()=<>]+|[0-9]+(?:[.][0-9]+)?)", text) 
+        result = ""
+        for part in parts:
+            part = part.strip()
+            if not part:
+                continue
+
+            if re.match(r"^[+\-*\/^()=<>]+|[0-9]+(?:[.][0-9]+)?$", part): 
+                result += part  
+            else:
+                result += r"\text{" + part + r"}" 
+
+        return f"${result}$"
